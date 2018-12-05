c90be44b2162abfb3beb4689400b020ee7fadfc8
zaid <zaidcomps@hotmail.com>
2018-12-05 12:14:59 +0530

    "Added some code to the model file"

--- django_projects/locallibrary/catalog/models.py
+++ django_projects/locallibrary/catalog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Genre(models.Model):
+    """Models representing a book genre"""
+    name = models.CharField(max_length=200, help_text='Enter a book genre (eg. Science Fiction)')
+
+    def __str__(self):
+        """String for representing Genre object"""
+        return self.name
+        
