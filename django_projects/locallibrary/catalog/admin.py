168a13dd8affe3a62b768670896635ce9d88ce07
zaid <zaidcomps@hotmail.com>
2018-12-06 00:21:40 +0530

    Added refactroing tool and added some records in the database which actually doesn't matter for code pov

--- django_projects/locallibrary/catalog/admin.py
+++ django_projects/locallibrary/catalog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from catalog.models import Book, Author, Genre, BookInstance, Language
+
+admin.site.register(Book)
+admin.site.register(Author)
+admin.site.register(Genre)
+admin.site.register(BookInstance)
+admin.site.register(Language)
+
+
--- django_projects/locallibrary/catalog/models.py
+++ django_projects/locallibrary/catalog/models.py
@@ -33,7 +33,7 @@ from django.urls import reverse #Used to generate URLS by reversing the URL patt
 
 class Book(models.Model):
     """Model representing a book (but not a specific copy of a book)."""
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=200, help_text='The title of the book, obvious!')
 
     # Foreign Key used because book can only have one author(Assumption), but authors can have multiple books
     # Author as a string rather than object because it hasn't been declared yet in the file
